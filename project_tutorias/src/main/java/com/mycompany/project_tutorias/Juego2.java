ff62151c5db405476af201ccb8dab22f93fe6cc7
Eleazar Neftalí Colop Colop <99382541+Eleazar11@users.noreply.github.com>
2026-01-29 17:41:26 -0600

    primer clase

---
+++ project_tutorias/src/main/java/com/mycompany/project_tutorias/Juego2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project_tutorias;
+
+/**
+ *
+ * @author eleaz
+ */
+public class Juego2 {
+        public static void ejecucion(){
+        System.out.println("Juego 2 en ejecucion");
+    }
+}
---
+++ project_tutorias/src/main/java/com/mycompany/project_tutorias/Project_tutorias.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_tutorias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eleaz
+ */
+public class Project_tutorias { 
+
+    public static void main(String[] args) {
+        String nombre;
+        int opcionMenu;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello World!");
+        saludar();
+        System.out.println("Ingrese su nombre");
+        nombre = scanner.nextLine();
+        saludar(nombre);
+        opcionMenu=1;
+     
+            switch (opcionMenu){
+                case 1:
+                    Juego1.ejecucion();
+                    break;
+                case 2:
+                    Juego2.ejecucion();
+                    break;
+                case 3: 
+                    Juego3.ejecucion();
+                    break;
+                case 0:
+                    System.out.println("Gracias por jugar. ¡Hasta luego!");
+                    
+                    break;
+                default:
+                    System.out.println("Este numero no cumple con ninguna opcion");
+            }
+       
+    }
+
+    //METODO PARA SALUDAR
+     public static void saludar() {
+        System.out.println("ESTE METODO ESTA SALUDANDO");
+    }
+    
+     //METODO PARA SALUDAR CUANDO ENVIAMOS DATOS
+    public static void saludar(String nombre) {
+        System.out.println("Bienvenido al juego " + nombre);
+    }
+    
+}
